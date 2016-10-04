fec8c489f5008fa9c526ae8c19019493751b85bf
edisonskiploop <edison@skiploop.com>
2016-10-04 10:46:23 +0530

    Home Layout

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 /**
- * Created by dsouz on 03-10-2016.
+ * Created by Edison D'souza on 03-10-2016.
  */
+var app = angular.module('lazeez-e-diyaaz', []);
+
+app.controller('mainCtrl', function($scope){
+    $scope.navlinks = [{name : 'HOME', link : '#'},
+        {name : 'ABOUT', link : '#'},
+        {name : 'SERVICES', link : '#'},
+        {name : 'MENU', link : '#'},
+        {name : 'GALLERY', link : '#'},
+        {name : 'TESTIMONIALS', link : '#'},
+        {name : 'CONTACT', link : '#'}];
+});
