76e1dbec40ab7b53031f1a262e17e0fec5bf1aa2
edisonskiploop <edison@skiploop.com>
2016-10-19 16:48:22 +0530

    Parallax Effect on All Services Page

--- appJQ.js
+++ appJQ.js
@@ -4,10 +4,15 @@
 
 $(function () {
 //PARALLAX
-//    if ($(window).outerWidth() > 768)
-//        $(window).on('scroll', function () {
-//            $('.parallax-wrapper').css('marginTop', ($(window).scrollTop() * -.05));
-//        });
+    if ($(window).outerWidth() > 768)
+        $(window).on('scroll', function () {
+            setTimeout(function(){
+                $('.parallax1').css('backgroundPositionY', (Number($(window).scrollTop() * 0.05) + '%') );
+                $('.parallax2').css('backgroundPositionY', (Number($(window).scrollTop() * 0.05) + '%') );
+            }, 100);
+
+
+        });
 
 
 });
